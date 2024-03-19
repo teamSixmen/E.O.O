9179bfa9307c574c87cdad76d0ca64f765ff1f58
ChanWool Lim <cksdnf90@gmail.com>
2024-03-19 18:24:50 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/screen1/Home';
 import MainMenu from './pages/screen2/MainMenu';
 import AdditionalMenu from './pages/screen3/AdditionalMenu' ;
 import Payment from './pages/screen4/Payment';
 import Settlement from './pages/screen5/Settlement';
 import Finish from './pages/screen6/Finish';
+import Layout from './layouts/Layout';
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home/>}/>
-          <Route path='/menu' element={<MainMenu/>}/>
-          <Route path='/additional' element={<AdditionalMenu/>}/>
-          <Route path='/payment' element={<Payment/>}/>
-          <Route path='/settlement' element={<Settlement/>}/>
-          <Route path='/goodbye' element={<Finish/>}/>
+          <Route index element={<Layout/>}>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/menu' element={<MainMenu/>}/>
+            <Route path='/additional' element={<AdditionalMenu/>}/>
+            <Route path='/payment' element={<Payment/>}/>
+            <Route path='/settlement' element={<Settlement/>}/>
+            <Route path='/goodbye' element={<Finish/>}/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
